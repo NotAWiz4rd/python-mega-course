e60e9e6093f907374cf6f089369765c5d4e90342
notawiz4rd <github@notawiz4rd.com>
2025-03-06 22:38:51 +0100

    day 4

--- todoapp/main.py
+++ todoapp/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, or show, or exit: ").strip().lower()
+    user_action = input("Type add, or show, edit or exit: ").strip().lower()
 
     match user_action:
         case 'add':
@@ -10,6 +10,11 @@ while True:
         case 'show':
             for todo in todos:
                 print(todo.title())
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            existing_todo_index = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[existing_todo_index] = new_todo
         case 'exit':
             break
         case whatever:
